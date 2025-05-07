722fc0c62ee962da7d8dfa9896bb801e8997513b
cacciabella12 <cacciabella12@gmail.com>
2025-05-07 13:52:33 +0200

    ii

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -11,11 +11,10 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "http://localhost:3000", // URL del frontend in sviluppo
-                    "https://billitbackend-production.up.railway.app" // URL del backend in produzione
+                    "http://localhost:3000"// URL del backend in produzione
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Metodi HTTP permessi
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "Accept") // Headers permessi
-                .allowCredentials(true); // Consente invio di cookie o header di autenticazione
+                .allowCredentials(false); // Consente invio di cookie o header di autenticazione
     }
 }
